8ca5ce4f8bd3f97c84628c26cfc79e7132f1984c
Wojciech <wojciech.wantuch@gmail.com>
2021-12-07 18:59:04 +0100

    View for adding room to database View for showing all rooms

--- Booking/views.py
+++ Booking/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
+
+from Booking import models
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, 'add_room.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        if name == '':
+            return render(request, 'add_room.html', {'error': 'Musisz podać nazwę sali konferencyjnej.'})
+        try:
+            rooms = models.Rooms.objects.get(name__iexact=name)
+            return render(request, 'add_room.html', {'error': 'Musisz podać unikatową nazwę sali konferencyjnej.'})
+        except ObjectDoesNotExist:
+            pass
+        
+        capacity = request.POST.get('capacity')
+        if int(capacity) < 1:
+            return render(request, 'add_room.html', {'error': 'Pojemność sali konferencyjnej nie może być ujemna.'})
+        
+        if request.POST.get('projector') == None:
+            projector = False
+        else:
+            projector = True
+
+        models.Rooms.objects.create(name=name, capacity=capacity, projector=projector)
+
+        return redirect('/')
+
+class ShowRooms(View):
+    def get(self, request):
+        try:
+            rooms = models.Rooms.objects.all()
+        except ObjectDoesNotExist:
+            return HttpResponse('Brak dostępnych sal!')
+
+        return render(request, 'rooms.html', {'rooms': rooms})
