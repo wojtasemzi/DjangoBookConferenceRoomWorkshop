b0ed03408901355ac15557ce3b7466a742514b62
Wojciech <wojciech.wantuch@gmail.com>
2021-12-07 18:57:14 +0100

    Model for Rooms table

--- Booking/models.py
+++ Booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Rooms(models.Model):
+    name = models.fields.CharField(max_length=255, unique=True)
+    capacity = models.fields.IntegerField()
+    projector = models.fields.BooleanField()
+
+    def __str__(self) -> str:
+        return f'{self.name}'
