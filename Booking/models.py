d948b64f82bd17ecf1f2000e3aa51f25a40039dd
Wojciech <wojciech.wantuch@gmail.com>
2021-12-08 16:33:25 +0100

    Add model for Reservations table

--- Booking/models.py
+++ Booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Rooms(models.Model):
     name = models.fields.CharField(max_length=255, unique=True)
@@ -7,3 +8,11 @@ class Rooms(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+class Reservations(models.Model):
+    date = models.fields.DateField()
+    room = models.ForeignKey(Rooms, on_delete=CASCADE)
+    comment = models.fields.TextField()
+
+    class Meta:
+        unique_together = [['date', 'room_id']]
